7c40974ce62b727eb45029c76848d4ad1d56c54a
Daisuke <a1485@icloud.com>
2021-08-19 19:23:16 +0900

    不要部分の修正

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -3,23 +3,17 @@
 score = ARGV[0]
 scores = score.split(',')
 shots = []
-
 scores.each do |s|
-  case s
-  when 'X'
+  if s == "X"
     shots << 10
     shots << 0
-  when 'S'
-    shots << 10
   else
     shots << s.to_i
   end
 end
 
 frames = []
-shots.each_slice(2) do |s|
-  frames << s
-end
+frames = shots.each_slice(2).to_a
 
 point = 0
 frames[0..8].each_with_index do |frame, i|
