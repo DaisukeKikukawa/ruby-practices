1c1db9df8f74789fcf50cb56c9682ab6cb52df8a
Daisuke <a1485@icloud.com>
2021-09-28 19:45:37 +0900

    標準入力とlsコマンドをつないだ際の-lオプションの対応

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -3,61 +3,61 @@
 require 'optparse'
 options = ARGV.getopts('l')
 
-filename = ARGV
+filenames = ARGV
 
-if filename.count.zero?
+lines_sum = 0
+words_sum = 0
+bytes_sum = 0
+
+if filenames.count.zero? && options['l']
+  input = $stdin.readlines
+  puts input.size.to_s.rjust(8)
+elsif filenames.count.zero?
   input = $stdin.readlines
   print input.size.to_s.rjust(8)
   print input.join.split(/\s+/).count.to_s.rjust(8)
   puts input.join.bytesize.to_s.rjust(8)
 end
 
-lines_sum = 0
-words_sum = 0
-bytes_sum = 0
-
-if options['l']
-  filename.each do |n|
+if filenames.count == 1 && options['l']
+  filenames.each do |n|
     file = File.read(n)
     print file.lines.count.to_s.rjust(8)
     puts " #{n}"
-
-    lines_sum += file.lines.count
   end
-
-  unless filename.count.zero?
-    print lines_sum.to_s.rjust(8)
-    puts ' total'
+elsif filenames.count == 1
+  filenames.each do |n|
+    file = File.read(n)
+    print file.lines.count.to_s.rjust(8)
+    print file.split(/\s+/).count.to_s.rjust(8)
+    print file.bytesize.to_s.rjust(8)
+    puts " #{n}"
   end
-
 end
 
-unless options['l']
-  if filename.count == 1
-    filename.each do |n|
-      file = File.read(n)
-      print file.lines.count.to_s.rjust(8)
-      print file.split(/\s+/).count.to_s.rjust(8)
-      print file.bytesize.to_s.rjust(8)
-      puts " #{n}"
-    end
+if filenames.count > 1 && options['l']
+  filenames.each do |n|
+    file = File.read(n)
+    print file.lines.count.to_s.rjust(8)
+    puts " #{n}"
+    lines_sum += file.lines.count
   end
+  print lines_sum.to_s.rjust(8)
+  puts ' total'
+elsif filenames.count > 1
+  filenames.each do |n|
+    file = File.read(n)
+    print file.lines.count.to_s.rjust(8)
+    print file.split(/\s+/).count.to_s.rjust(8)
+    print file.bytesize.to_s.rjust(8)
+    puts " #{n}"
 
-  if filename.count > 1
-    filename.each do |n|
-      file = File.read(n)
-      print file.lines.count.to_s.rjust(8)
-      print file.split(/\s+/).count.to_s.rjust(8)
-      print file.bytesize.to_s.rjust(8)
-      puts " #{n}"
-
-      lines_sum += file.lines.count
-      words_sum += file.split(/\s+/).count
-      bytes_sum += file.bytesize
-    end
-    print lines_sum.to_s.rjust(8)
-    print words_sum.to_s.rjust(8)
-    print bytes_sum.to_s.rjust(8)
-    puts ' total'
+    lines_sum += file.lines.count
+    words_sum += file.split(/\s+/).count
+    bytes_sum += file.bytesize
   end
+  print lines_sum.to_s.rjust(8)
+  print words_sum.to_s.rjust(8)
+  print bytes_sum.to_s.rjust(8)
+  puts ' total'
 end
