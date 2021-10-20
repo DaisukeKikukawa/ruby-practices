7c898f506a5fc7a36887a4dc258677f24dd7f20a
Daisuke <a1485@icloud.com>
2021-10-20 21:42:45 +0900

    条件分岐の修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -9,55 +9,43 @@ lines_sum = 0
 words_sum = 0
 bytes_sum = 0
 
-if filenames.count.zero? && options['l']
-  input = $stdin.readlines
-  puts input.size.to_s.rjust(8)
-elsif filenames.count.zero?
-  input = $stdin.readlines
-  print input.size.to_s.rjust(8)
-  print input.join.split(/\s+/).count.to_s.rjust(8)
-  puts input.join.bytesize.to_s.rjust(8)
-end
+if filenames.count >= 1
+  if options['l']
+    filenames.each do |n|
+      file = File.read(n)
+      print file.lines.count.to_s.rjust(8)
+      puts "#{n}"
+      lines_sum += file.lines.count
+    end
+    if filenames.count > 1
+      print lines_sum.to_s.rjust(8)
+      puts 'total'
+    end
+  else
+    filenames.each do |n|
+      file = File.read(n)
+      print file.lines.count.to_s.rjust(8)
+      print file.split(/\s+/).count.to_s.rjust(8)
+      print file.bytesize.to_s.rjust(8)
+      puts "#{n}"
 
-if filenames.count == 1 && options['l']
-  filenames.each do |n|
-    file = File.read(n)
-    print file.lines.count.to_s.rjust(8)
-    puts " #{n}"
+      lines_sum += file.lines.count
+      words_sum += file.split(/\s+/).count
+      bytes_sum += file.bytesize
+    end
+    print lines_sum.to_s.rjust(8)
+    print words_sum.to_s.rjust(8)
+    print bytes_sum.to_s.rjust(8)
+    puts 'total'
   end
-elsif filenames.count == 1
-  filenames.each do |n|
-    file = File.read(n)
-    print file.lines.count.to_s.rjust(8)
-    print file.split(/\s+/).count.to_s.rjust(8)
-    print file.bytesize.to_s.rjust(8)
-    puts " #{n}"
-  end
-end
-
-if filenames.count > 1 && options['l']
-  filenames.each do |n|
-    file = File.read(n)
-    print file.lines.count.to_s.rjust(8)
-    puts " #{n}"
-    lines_sum += file.lines.count
-  end
-  print lines_sum.to_s.rjust(8)
-  puts ' total'
-elsif filenames.count > 1
-  filenames.each do |n|
-    file = File.read(n)
-    print file.lines.count.to_s.rjust(8)
-    print file.split(/\s+/).count.to_s.rjust(8)
-    print file.bytesize.to_s.rjust(8)
-    puts " #{n}"
-
-    lines_sum += file.lines.count
-    words_sum += file.split(/\s+/).count
-    bytes_sum += file.bytesize
-  end
-  print lines_sum.to_s.rjust(8)
-  print words_sum.to_s.rjust(8)
-  print bytes_sum.to_s.rjust(8)
-  puts ' total'
+else
+    if options['l']
+      input = $stdin.readlines
+      puts input.size.to_s.rjust(8)
+    else
+      input = $stdin.readlines
+      print input.size.to_s.rjust(8)
+      print input.join.split(/\s+/).count.to_s.rjust(8)
+      puts input.join.bytesize.to_s.rjust(8)
+    end
 end
