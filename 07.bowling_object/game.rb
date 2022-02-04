4a5f59c4c865c49b31a9fb62613fb1cc8b81ba44
Daisuke <a1485@icloud.com>
2022-02-04 13:16:41 +0900

    各frameを計算するメソッドの追加

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -4,6 +4,21 @@ require_relative 'shot'
 require_relative 'frame'
 
 class Game
-def initialize(input)
-end
+  def initialize(input)
+    @input = input
+    frames = []
+    one_frame_pinfall = []
+    @input.split(',').each do |shot|
+      one_frame_pinfall << shot
+      if frames.size < 10
+        if shot == 'X' || one_frame_pinfall.size >= 2
+          frames << one_frame_pinfall.clone
+          one_frame_pinfall.clear
+        end
+      else
+        frames.last << shot
+      end
+    end
+    @frames = frames.map { |frame| Frame.new(*frame) }
+  end
 end
