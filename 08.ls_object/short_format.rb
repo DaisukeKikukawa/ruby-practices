82f852a5827d3bdcdda66cc9093dd4bdb539ac4f
DaisukeKikukawa <daisuke.kikukawa.16@gmail.com>
2022-08-04 17:28:40 +0900

    add: formatクラス

---
+++ 08.ls_object/short_format.rb
@@ -0,0 +1,12 @@
+class ShortFormat
+  attr_reader :files
+
+  def initialize(files)
+    @files = files
+  end
+
+  def output
+    files_array = files.map { |x| x.to_s.ljust(20) }.each_slice(6).to_a
+    files_array[0].zip(*files_array[1..-1]).each { |x| puts x.join(' ') }
+  end
+end
