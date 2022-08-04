17f5bf61cc4f7fc569b6baff570796ee0e2be55e
DaisukeKikukawa <daisuke.kikukawa.16@gmail.com>
2022-08-04 15:42:05 +0900

    add: formatクラス

---
+++ 08.ls_object/long_format.rb
@@ -0,0 +1,49 @@
+require_relative 'ls_file'
+class LongFormat
+  attr_reader :files
+
+  def initialize(files)
+    @files = files
+  end
+
+  def l_option
+    # lオプションtotal
+    total
+    # lオプション詳細
+    l_option_detail
+  end
+
+  def l_option_detail
+    files.each do |file|
+      stat = File::Stat.new(file)
+
+      data = [
+        stat.mode.to_s(8).rjust(6).chars,
+        stat.nlink.to_s.rjust(2),
+        Etc.getpwuid(File.stat(file).uid).name,
+        Etc.getgrgid(File.stat(file).gid).name,
+        stat.size.to_s.rjust(5),
+        stat.ctime.strftime('%m %e %k:%M').gsub(/^0/, ''),
+        file
+      ]
+      # lオプション詳細出力
+      file_type_atribute = LsFile.new(data).file_type
+      file_type_atribute << LsFile.new(data).permission
+      data[0] = file_type_atribute
+      puts data.join(' ')
+    end
+  end
+
+  private
+
+  def total
+    count = []
+    # totalの表示
+    files.each do |file|
+      stat = File::Stat.new(file)
+      count << stat.blocks
+    end
+    puts "total #{count.sum}"
+  end
+end
+
